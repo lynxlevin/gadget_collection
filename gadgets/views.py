6eb4f222c93c50c5d92ec7b7c178006cf970ea04
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2022-03-17 06:45:57 +0900

    gadgets/index page

--- gadgets/views.py
+++ gadgets/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from gadgets.models import Gadget
 
 
-def index(reqest):
-    return HttpResponse('hello world')
+class IndexView(generic.ListView):
+    template_name = 'gadgets/index.html'
+    context_object_name = 'gadget_list'
+
+    def get_queryset(self):
+        return Gadget.objects.select_related()
